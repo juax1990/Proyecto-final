c1735031438cb2f2d2b4c7b62e587a0442422e4b
Jhon Tumiña <juax1990@hotmail.com>
2025-09-19 20:16:49 -0500

    Primer commit con las ediciones a la plantilla de la pagina

---
+++ basedatos/datos.js
@@ -0,0 +1,31 @@
+
+//Lista que contiene los elementos que se van a mostrar en el carrusel de la pagina principal
+const elementos_carrusel = [
+    {nombre:"Diseño Grafico", 
+    descripcion:"Elaboramos y creamos el diseño de tu marca, como la creación de logo, manuales corporativos, diseños y catálogos para que muestres tus productos o servicios.",
+    imagen:"img/brochure.png"},
+    {nombre:"Social media", 
+    descripcion:"Diseñamos tus post, stories para el feed de tus redes sociales para que se vea de una forma atractiva y corporativa.",
+    imagen:"img/redes-divine.jpg"},
+    {nombre:"P.O.P", 
+    descripcion:"Aplicamos tu marca a diferentes piezas publicitarias obteniendo espectaculares resultados en avisos, habladores QR, señalética y muchos más.",
+    imagen:"img/AVISO-EN-ACRILICO-3D-BONITA-COSMETICS.png"},
+    {nombre:"Papeleria comercial", 
+    descripcion:"Diseñamos e imprimimos la papelería comercial de tu empresa, incluyendo talonarios, hojas membretadas, tarjetas de presentación y más.",
+    imagen:"img/2.jpg"},
+]
+
+const lista_categorias = [
+    {nombre:"Aviso tipo boton", 
+    descripcion:"aviso circular en la que destaca tu marca y decora los espacios de tu local.",
+    imagen:"img/AVISO-TIPO-BOTON.png"},
+    {nombre:"Pines", 
+    descripcion:"formas en acrilico ideales para destacar tu trabajo de uñas y exhibir tu marca y conectacto.",
+    imagen:"img/PINES.png"},
+    {nombre:"Habladores QR", 
+    descripcion:"ideal para aplicar qr como formas de pago, redes sociales menu o lo que desees facilitando a tus clientes el acceso.",
+    imagen:"img/hablador.png"},
+    {nombre:"Cajas en acrilico", 
+    descripcion:"elaboradas en acrilico ideal para desatcar tu marca desde la parte externa , elaborados con elementos en 3D, iluminación y detalles en acrilico espejo.",
+    imagen:"img/2.jpg"},
+]
